b3c3642884a98e765d076bd236be0417e48dbb10
nekc641 <saekoyarezu09@gmail.com>
2025-04-19 02:40:18 +0800

    Update setup.py

--- modules/setup.py
+++ modules/setup.py
@@ -7,12 +7,12 @@ def setup():
         with open('config.ini', 'w') as file:
             file.write("""[bot_nuker_config]
 bot_token = YOUR_BOT_TOKEN
-spam_message = @everyone NUKED BY https://github.com/FlashGriefs/Hazus
-role_name = Hazus
-channel_name = HAZUS ON TOP
-dm_message = A SERVER YOU WERE IN WAS NUKED BY HAZUS NUKER https://github.com/FlashGriefs/Hazus
-ban_message = NUKED BY https://github.com/FlashGriefs/Hazus
-server_name = HAZUS OWNS U""")
+spam_message = @everyone luh sino to? https://cdn.discordapp.com/attachments/1362832473607901456/1362854931345707290/IMG_20250419_004156_894.jpg?ex=6803e910&is=68029790&hm=232ba72fe832a14d8a5484d2d645bcbe156f8810a149279ed9979880ab369147&
+role_name = Tekk
+channel_name = TEKO
+dm_message = SALOT TONG BABAENG TO HAHAHAHAHA https://cdn.discordapp.com/attachments/1362832473607901456/1362854931345707290/IMG_20250419_004156_894.jpg?ex=6803e910&is=68029790&hm=232ba72fe832a14d8a5484d2d645bcbe156f8810a149279ed9979880ab369147&
+ban_message = 
+server_name = TEKO OWNS U""")
     if not os.path.exists('proxies.txt'):
         cprint("proxies.txt file does not exist, creating one for you...", 3)
         with open('proxies.txt', 'w') as file:
